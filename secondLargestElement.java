9f60177a6813847dc6cefc74fae1dd7ed35393b1
Kshitij Pratap Tomer <kshitijpratap2004@gmail.com>
2023-06-25 10:45:29 +0530

    Added striver code

---
+++ secondLargestElement.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class secondLargestElement {
+    static int secon_largest(int [] arr){
+
+        return -1;
+    }
+    public static void main(String[] args) {
+
+    }
+}
