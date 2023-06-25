50c0f4a294720fc79a7c51f42010cab575fbcac5
Kshitij Pratap Tomer <kshitijpratap2004@gmail.com>
2023-06-25 11:08:04 +0530

    Added code of finding missing postive nos

---
+++ findMissingPositive.java
@@ -0,0 +1,42 @@
+package sorting;
+
+public class findMissingPositive {
+    public static int firstMissingPositive(int [] nums){
+        int i = 0;
+        while (i < nums.length){
+            int correct = nums[i] - 1;
+            if (nums[i] > 0 && nums[i] < nums.length && nums[i] != nums[correct]){
+                swap(nums,i, correct);
+            }
+            else{
+                i++;
+            }
+        }
+        for (int index = 0; index < nums.length; index++){
+            if (nums[index] != index){
+                return index + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+    static void swap(int [] arr, int start, int end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+    static void cyclic_Sort(int [] nums){
+        int i = 0;
+        while (i < nums.length){
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]){
+                swap(nums,i, correct);
+            }
+            else{
+                i++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
