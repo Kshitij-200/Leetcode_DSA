961e37a3d2571593eda88547a051039cbee501c8
Kshitij Pratap Tomer <kshitijpratap2004@gmail.com>
2023-06-25 10:46:45 +0530

    Added binary search application code

---
+++ searchRoatatedArray.java
@@ -0,0 +1,22 @@
+package search;
+
+public class searchRoatatedArray {
+    public static void search_rotated(int [] arr, int target){
+
+    }
+
+    static int findPivot(int [] arr){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+
+
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+
+    }
+}
