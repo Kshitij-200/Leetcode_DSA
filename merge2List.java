95daa325d31e1241ac1f00d28b1b79e0d5c7d50a
Kshitij Pratap Tomer <kshitijpratap2004@gmail.com>
2023-07-08 14:01:12 +0530

    Added code to merge 2 lists

---
+++ merge2List.java
@@ -0,0 +1,24 @@
+package LinkedList;
+
+public class merge2List {
+    Node head;
+    Node tail;
+    int size;
+    class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+        Node(int data, Node next){
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
