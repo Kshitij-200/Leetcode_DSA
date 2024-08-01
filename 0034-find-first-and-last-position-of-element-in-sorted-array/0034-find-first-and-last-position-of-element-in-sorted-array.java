2d501d76cc7e94306ca321f9cae1be00dc734df2
Kshitij Pratap Tomer <93885290+Kshitij-200@users.noreply.github.com>
2024-08-01 17:09:03 +0530

    Time: 0 ms (100%), Space: 46.4 MB (7.73%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,35 +1,42 @@
 class Solution {
-   public int [] searchRange(int [] nums, int target) {
-        int[] ans = {-1, -1};
+    public int[] searchRange(int[] nums, int target) {
+        int pehla = first(nums,target,0,nums.length - 1);
+        int dosra = second(nums,target,0,nums.length - 1);
 
-        // check for first occurence of the target
-        int start = search(nums,target,true);
-        int end = search(nums,target,false);
-
-        ans[0] = start;
-        ans[1] = end;
-        return ans;
+        return new int[]{pehla,dosra};
     }
-    int search(int [] nums, int target, boolean findsStartIndex){
-        int ans = -1;
 
-        int start = 0;
-        int end = nums.length - 1;
-        int size = nums.length;
-        while (start<=end){
-            int mid = start + (end-start)/2;
-            if (target > nums[mid]){
+    public int first (int [] nums, int target, int start, int end){
+        int ans = -1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(target > nums[mid]){
                 start = mid + 1;
-            } else if (target < nums[mid]) {
-                end = mid-1;
             }
             else{
+                end = mid - 1;
+            }
+            if(nums[mid] == target){
                 ans = mid;
-                if(findsStartIndex){
-                    end = mid-1;
-                }else {
-                    start = mid+1;
-                }
+            }
+        }
+        return ans;
+    }
+
+    public int second (int [] nums, int target, int start, int end){
+        int ans = -1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(target < nums[mid]){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+            if(nums[mid] == target){
+                ans = mid;
             }
         }
         return ans;
